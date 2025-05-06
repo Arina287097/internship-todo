a9d07b6019d716d9f0971a69d4e3f7835db6f80c
Арина Гриценко <gricena2007@gmail.com>
2025-05-06 16:18:12 +0300

    исправление ошибок

--- student_623042/Student.ConsoleTodo/Student.ConsoleTodo/Program.cs
+++ student_623042/Student.ConsoleTodo/Student.ConsoleTodo/Program.cs
@@ -2,15 +2,17 @@
 {
     class Program
     {
+        //список задач
         static List<Task> tasks = new List<Task>();
 
         static void Main(string[] args)
         {
+            //основоной цикл программы
             while (true)
             {
                 ShowMenu();
                 int choice = GetMenuChoice();
-
+                //выбор пользователя
                 switch (choice)
                 {
                     case 1:
@@ -18,31 +20,36 @@
                         break;
                     case 2:
                         AddTask();
-                        break;
-                    default:
-                        Console.WriteLine("Неверный ввод, попробуйте снова.");
-                        break;
+                        break; 
                 }
             }
         }
-
+        //метод отображения меню
         static void ShowMenu()
         {
             Console.Clear();
             Console.WriteLine("Для вызова выполняемой подпрограммы укажите ее номер и нажмите Enter:");
             Console.WriteLine("1 - Посмотреть список задач");
             Console.WriteLine("2 - Добавить задачу");
-            Console.WriteLine("Esc - Выйти");
         }
-
+        //метод для получения выбора пользователя
         static int GetMenuChoice()
         {
-            return Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    return Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
+            }
         }
-
+        //метод для добавления задач
         static void AddTask()
         {
-            Console.Clear();
             Console.WriteLine("Введите заголовок задачи. По завершению ввода нажмите Enter:");
             string title = Console.ReadLine();
 
@@ -53,31 +60,37 @@
             Console.WriteLine("Задача добавлена! Нажмите Enter для продолжения.");
             Console.ReadLine();
         }
-
+        //метод для просмотра задач
         static void ShowTasks()
         {
-            Console.Clear();
-            Console.WriteLine("Список задач:");
+            while (true)
+            {
+                Console.WriteLine("Список задач:");
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
 
-            if (tasks.Count == 0)
-            {
-                Console.WriteLine("Задач нет.");
-            }
-            else
-            {
-                for (int i = 0; i < tasks.Count; i++)
+                if (tasks.Count == 0)
                 {
-                    Console.WriteLine($"Задача {i + 1}:");
-                    Console.WriteLine($"Заголовок: {tasks[i].Title}");
-                    Console.WriteLine($"Описание: {tasks[i].Description}");
+                    Console.WriteLine("Задач нет.");
+                }
+                else
+                {
+                    for (int i = 0; i < tasks.Count; i++)
+                    {
+                        Console.WriteLine($"Задача {i + 1}:");
+                        Console.WriteLine($"Заголовок: {tasks[i].Title}");
+                        Console.WriteLine($"Описание: {tasks[i].Description}");
+                    }
+                }
+
+                if (keyInfo.Key == ConsoleKey.Escape)
+                {
+                    Console.WriteLine("Вы в меню");
+                    break;
                 }
             }
-
-            Console.WriteLine("Для возврата к списку подпрограмм нажмите Esc:");
-            Console.ReadKey(true);
         }
     }
-
+    //класс для представления задач
     class Task
     {
         public string Title { get; set; }
