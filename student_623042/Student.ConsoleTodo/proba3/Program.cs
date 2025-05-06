884c944e2f3b00974d13adf26473b3054e1b2a9a
Арина Гриценко <gricena2007@gmail.com>
2025-05-06 15:32:32 +0300

    #исправление

---
+++ student_623042/Student.ConsoleTodo/proba3/Program.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            ShowMenu();
+            int choice = GetMenuChoice();
+
+            switch (choice)
+            {
+                //if else
+                case 1:
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию If Else");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey();
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите число для проверки:");
+                                string input = Console.ReadLine();  
+
+                                if (string.IsNullOrEmpty(input))
+                                {
+                                    Console.WriteLine("Вы ничего не ввели. Попробуйте снова.");
+                                    continue;
+                                }
+
+                                if (int.TryParse(input, out int number))
+                                {
+                                    if (number % 2 == 0)
+                                    {
+                                        Console.WriteLine($"Число {number} четное.");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"Число {number} нечетное.");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ошибка: введено не целое число.");
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                                continue;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+                       
+                    }
+                    break;
+                //while
+                case 2:
+                    int count = 0;
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию While");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите число для проверки");
+                                if (!int.TryParse(Console.ReadLine(), out count))
+                                {
+                                    Console.WriteLine("Введите корректное число.");
+                                    continue;
+                                }
+                                int i = 0;
+                                while (i < count)
+                                {
+                                    Console.WriteLine(i + 1);
+                                    i++;
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                                continue;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+                    }
+                    break;
+                //do while 
+                case 3:
+                    int repetitions = 0;
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию While Do");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите число для проверки");
+                                if (!int.TryParse(Console.ReadLine(), out repetitions))
+                                {
+                                    Console.WriteLine("Введите корректное число.");
+                                    continue;
+                                }
+                                int g = 0;
+                                do
+                                {
+                                    Console.WriteLine(g + 1);
+                                    g++;
+                                } while (g < repetitions);
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                                continue;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+                    }
+                    break;
+                //for
+                case 4:
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию For");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите Начальное число:");
+                                int start = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Введите Количество повторений:");
+                                int repeats = Convert.ToInt32(Console.ReadLine());
+
+                                Console.WriteLine("Результат:");
+
+                                for (int i = start; i < start + repeats; i++)
+                                {
+                                    Console.WriteLine(i);
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                                continue;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+                    }
+                    break;
+                //foreach
+                case 5:
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию Foreach");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите числа через пробел для проверки");
+                                string[] input = Console.ReadLine().Split(' ');
+                                Console.WriteLine("Результат:");
+                                foreach (var item in input)
+                                {
+                                    Console.WriteLine(item);
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                                continue;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+
+                    }
+                    break;
+                //switch
+                case 6:
+                    while (true)
+                    {
+                        Console.WriteLine("вы выбрали инструкцию Switch");
+                        Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+                        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                        if (keyInfo.Key == ConsoleKey.Enter)
+                        {
+                            try
+                            {
+                                Console.WriteLine("Введите номер дня недели");
+                                if (int.TryParse(Console.ReadLine(), out int day))
+                                {
+                                    string dayName;
+
+                                    switch (day)
+                                    {
+                                        case 1:
+                                            dayName = "Понедельник";
+                                            break;
+                                        case 2:
+                                            dayName = "Вторник";
+                                            break;
+                                        case 3:
+                                            dayName = "Среда";
+                                            break;
+                                        case 4:
+                                            dayName = "Четверг";
+                                            break;
+                                        case 5:
+                                            dayName = "Пятница";
+                                            break;
+                                        case 6:
+                                            dayName = "Суббота";
+                                            break;
+                                        case 7:
+                                            dayName = "Воскресенье";
+                                            break;
+                                        default:
+                                            dayName = "Некорректный ввод.";
+                                            break;
+                                    }
+                                    Console.WriteLine(dayName);
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Ошибка");
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.Escape)
+                        {
+                            Console.WriteLine("Вы в меню");
+                            break;
+                        }
+
+                    }
+                        break;   
+            }
+        }
+    }
+    static void ShowMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Для вызова выполняемой подпрограммы укажите ее номер и нажмите Enter:");
+        Console.WriteLine("1 - IF ELSE");
+        Console.WriteLine("2 - WHILE");
+        Console.WriteLine("3 - DO WHILE");
+        Console.WriteLine("4 - FOR");
+        Console.WriteLine("5 - FOREACH");
+        Console.WriteLine("6 - SWITCH");
+    }
+    static int GetMenuChoice()
+    {
+        return Convert.ToInt32(Console.ReadLine());
+    }
+}
--- student_623042/Student.SharpInstructions/Student.SharpInstructions/Program.cs
+++ student_623042/Student.SharpInstructions/Student.SharpInstructions/Program.cs
@@ -1,6 +1,8 @@
-﻿class Program
+﻿using System;
+
+class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         while (true)
         {
@@ -55,29 +57,50 @@
     {
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите целое число для проверки (для возврата к списку подпрограмм нажмите Esc):");
-            string input = Console.ReadLine();
-
-            if (string.IsNullOrEmpty(input)) continue;
-
-            if (int.TryParse(input, out int number))
+            Console.WriteLine("вы выбрали инструкцию If Else");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.WriteLine();
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                if (number % 2 == 0)
+                try
                 {
-                    Console.WriteLine("Число четное.");
+                    Console.WriteLine("Введите число для проверки:");
+                    string input = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("Вы ничего не ввели. Попробуйте снова.");
+                        continue;
+                    }
+
+                    if (int.TryParse(input, out int number))
+                    {
+                        if (number % 2 == 0)
+                        {
+                            Console.WriteLine($"Число {number} четное.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Число {number} нечетное.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка: введено не целое число.");
+                    }
                 }
-                else
+                catch
                 {
-                    Console.WriteLine("Число нечетное.");
+                    Console.WriteLine("Ошибка");
+                    continue;
                 }
             }
-            else
+            else if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                Console.WriteLine("Вы в меню");
+                break;
             }
-
-            if (!WaitForKey()) break; // Возврат в главное меню
         }
     }
     // While работает
@@ -86,41 +109,77 @@
         int count = 0;
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите число для запуска цикла (для возврата к списку подпрограмм нажмите Esc):");
+            Console.WriteLine("вы выбрали инструкцию While");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-            if (!int.TryParse(Console.ReadLine(), out count))
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                Console.WriteLine("Введите корректное число.");
-                continue;
+                try
+                {
+                    Console.WriteLine("Введите число для проверки");
+                    if (!int.TryParse(Console.ReadLine(), out count))
+                    {
+                        Console.WriteLine("Введите корректное число.");
+                        continue;
+                    }
+                    int i = 0;
+                    while (i < count)
+                    {
+                        Console.WriteLine(i + 1);
+                        i++;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                    continue;
+                }
             }
-
-            int i = 0;
-            while (i < count)
+            else if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Console.WriteLine(i + 1);
-                i++;
+                Console.WriteLine("Вы в меню");
+                break;
             }
-
-            if (!WaitForKey()) break; // Возврат в главное меню
         }
     }
     //while do работает
     static void ExecuteDoWhile()
     {
-        int count = 0;
+        int repetitions = 0;
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите количество повторений: ");
-            int repetitions = Convert.ToInt32(Console.ReadLine());
-            do
+            Console.WriteLine("вы выбрали инструкцию While");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                Console.WriteLine(count);
-                count++;
-            } while (count <= repetitions);
-
-            if (!WaitForKey()) break; // Возврат в главное меню
+                try
+                {
+                    Console.WriteLine("Введите число для проверки");
+                    if (!int.TryParse(Console.ReadLine(), out repetitions))
+                    {
+                        Console.WriteLine("Введите корректное число.");
+                        continue;
+                    }
+                    int g = 0;
+                    do
+                    {
+                        Console.WriteLine(g + 1);
+                        g++;
+                    } while (g < repetitions);
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                    continue;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Вы в меню");
+                break;
+            }
         }
     }
     //for работает
@@ -128,22 +187,36 @@
     {
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите начальное число и количество повторений (для возврата к списку подпрограмм нажмите Esc):");
-
-            Console.WriteLine("Начальное число:");
-            int start = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Количество повторений:");
-            int repeats = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Результат:");
-
-            for (int i = start; i < start + repeats; i++)
+            Console.WriteLine("вы выбрали инструкцию For");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                Console.WriteLine(i);
-            }
+                try
+                {
+                    Console.WriteLine("Введите Начальное число:");
+                    int start = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите Количество повторений:");
+                    int repeats = Convert.ToInt32(Console.ReadLine());
 
-            if (!WaitForKey()) break; // Возврат в главное меню
+                    Console.WriteLine("Результат:");
+
+                    for (int i = start; i < start + repeats; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                    continue;
+                }
+            }
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Вы в меню");
+                break;
+            }
         }
     }
     //foreach работает
@@ -151,15 +224,32 @@
     {
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите несколько чисел, разделенных пробелом:");
-            string[] input = Console.ReadLine().Split(' ');
-            Console.WriteLine("Результат:");
-            foreach (var item in input)
+            Console.WriteLine("вы выбрали инструкцию Foreach");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                Console.WriteLine(item);
+                try
+                {
+                    Console.WriteLine("Введите числа через пробел для проверки");
+                    string[] input = Console.ReadLine().Split(' ');
+                    Console.WriteLine("Результат:");
+                    foreach (var item in input)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                    continue;
+                }
             }
-            if (!WaitForKey()) break; // Возврат в главное меню
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Вы в меню");
+                break;
+            }
         }
     }
     //switch работает
@@ -167,66 +257,60 @@
     {
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("Введите номер дня недели (1-7, для возврата к списку подпрограмм нажмите Esc):");
-
-            if (int.TryParse(Console.ReadLine(), out int day))
+            Console.WriteLine("вы выбрали инструкцию Switch");
+            Console.WriteLine("нажмите Enter для запуска инструкции, для возврата к списку подпрограмм нажмите Esc");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            if (keyInfo.Key == ConsoleKey.Enter)
             {
-                string dayName;
-
-                switch (day)
+                try
                 {
-                    case 1:
-                        dayName = "Понедельник";
-                        break;
-                    case 2:
-                        dayName = "Вторник";
-                        break;
-                    case 3:
-                        dayName = "Среда";
-                        break;
-                    case 4:
-                        dayName = "Четверг";
-                        break;
-                    case 5:
-                        dayName = "Пятница";
-                        break;
-                    case 6:
-                        dayName = "Суббота";
-                        break;
-                    case 7:
-                        dayName = "Воскресенье";
-                        break;
-                    default:
-                        dayName = "Некорректный ввод.";
-                        break;
+                    Console.WriteLine("Введите номер дня недели");
+                    if (int.TryParse(Console.ReadLine(), out int day))
+                    {
+                        string dayName;
+
+                        switch (day)
+                        {
+                            case 1:
+                                dayName = "Понедельник";
+                                break;
+                            case 2:
+                                dayName = "Вторник";
+                                break;
+                            case 3:
+                                dayName = "Среда";
+                                break;
+                            case 4:
+                                dayName = "Четверг";
+                                break;
+                            case 5:
+                                dayName = "Пятница";
+                                break;
+                            case 6:
+                                dayName = "Суббота";
+                                break;
+                            case 7:
+                                dayName = "Воскресенье";
+                                break;
+                            default:
+                                dayName = "Некорректный ввод.";
+                                break;
+                        }
+                        Console.WriteLine(dayName);
+                    }
                 }
-
-                Console.WriteLine(dayName);
+                catch
+                {
+                    Console.WriteLine("Ошибка");
+                }
             }
-            else
+            else if (keyInfo.Key == ConsoleKey.Escape)
             {
-                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                Console.WriteLine("Вы в меню");
+                break;
             }
 
-            if (!WaitForKey()) break; // Возврат в главное меню
         }
     }
-    //esc enter работают
-    static bool WaitForKey()
-    {
-        Console.WriteLine("Нажмите Enter, чтобы повторить выполнение подпрограммы, или Esc, чтобы вернуться в главное меню.");
-        while (true)
-        {
-            var key = Console.ReadKey(true).Key;
-            if (key == ConsoleKey.Enter)
-            {
-                return true; // Повторить выполнение подпрограммы
-            }
-            else if (key == ConsoleKey.Escape)
-            {
-                return false; // Возврат в главное меню
-            }
-        }
-    }
+    
 }
