4a8e14d410bb7620102ac51d887856928fc6865c
Арина Гриценко <gricena2007@gmail.com>
2025-06-10 15:47:02 +0300

    Создание WebFormsTodos

---
+++ student_623042/Student/StudentTodo/Models/TodoTask.cs
@@ -0,0 +1,37 @@
+﻿namespace Student.Todo.Models
+{
+    /// <summary>
+    /// Задача
+    /// </summary>
+    public class TodoTask
+    {
+        /// <summary>
+        /// Заголовок задачи
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Описани задачи
+        /// </summary>
+        public string Description { get; set; }
+
+        // для инциализации объекта 
+        public TodoTask() { }
+
+        // Конструктор с параметрами name и description
+        public TodoTask(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        /// <summary>
+        /// Получить список задач
+        /// </summary>
+        /// <returns>Строковое значение задачи</returns>
+        public override string ToString()
+        {
+            return $"{Title} - {Description}";
+        }
+    }
+}
---
+++ student_623042/Student/StudentTodo/Services/TaskManager.cs
@@ -0,0 +1,36 @@
+﻿using Student.Todo.Models;
+
+namespace Student.Todo.Services
+{
+    /// <summary>
+    /// Диспетчер задач
+    /// </summary>
+    public class TaskManager
+    {
+        private List<TodoTask> tasks = new List<TodoTask>();
+
+        /// <summary>
+        /// Добавить задачу
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public void AddTask(TodoTask task)
+        {
+            tasks.Add(task);
+        }
+
+        /// <summary>
+        /// Удалить задачу
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public void RemoveTask(TodoTask task)
+        {
+            tasks.Remove(task);
+        }
+
+        /// <summary>
+        /// Получить список задач
+        /// </summary>
+        /// <returns>Список задач</returns>
+        public List<TodoTask> GetTasks() => tasks;
+    }
+}
